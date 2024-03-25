6e7f52bae96ce995c1426c24a93cea8c430753be
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2024-03-25 15:58:33 +0100

    added found_nl fucntion to utils

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,8 +6,28 @@
 /*   By: mamichal <mamichal@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/24 12:01:01 by mamichal          #+#    #+#             */
-/*   Updated: 2024/03/24 12:09:12 by mamichal         ###   ########.fr       */
+/*   Updated: 2024/03/25 15:47:28 by mamichal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./get_next_line.h"
+int	found_nl(t_list *list)
+{
+	int	i;
+
+	if (NULL == list)
+		return (0);
+	while (list)
+	{
+		i = 0;
+		while (list->content[i] && i < BUFFER_SIZE)
+		{
+			if (list->content[i] == '\n')
+				return (1);
+			i++;
+		}
+		list = list->next;
+	}
+	return (0);
+}
+
