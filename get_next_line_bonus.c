e66425dd6ce949b5617df5ac22a6e137486c4129
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2024-04-03 16:22:00 +0200

    added bonus
    added bonus to work with multiple file desctriptors

---
+++ get_next_line_bonus.c
@@ -0,0 +1,110 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mamichal <mamichal@student.42warsaw.pl>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/24 12:00:55 by mamichal          #+#    #+#             */
+/*   Updated: 2024/04/03 16:19:50 by mamichal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "./get_next_line_bonus.h"
+
+static void	list_append(t_list **list, char *buf, int fd)
+{
+	t_list	*last_node;
+	t_list	*new_node;
+
+	last_node = ft_lstlast(list[fd]);
+	new_node = (t_list *)malloc(sizeof(t_list));
+	if (NULL == new_node)
+		return ;
+	if (NULL == last_node)
+		list[fd] = new_node;
+	else
+		last_node->next = new_node;
+	new_node->content = (char *)buf;
+	new_node->next = NULL;
+}
+
+static void	alloc_list(t_list **list, int fd)
+{
+	int		bytes_read;
+	char	*buf;
+
+	while (!found_nl(list[fd]))
+	{
+		buf = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
+		if (NULL == buf)
+			return ;
+		bytes_read = read(fd, buf, BUFFER_SIZE);
+		if (!bytes_read)
+		{
+			free(buf);
+			return ;
+		}
+		buf[bytes_read] = 0;
+		list_append(list, buf, fd);
+	}
+}
+
+static char	*get_line(t_list *list)
+{
+	int		len;
+	char	*line;
+
+	if (NULL == list)
+		return (NULL);
+	len = len_to_nl(list);
+	line = (char *)malloc((len + 1) * sizeof(char));
+	if (NULL == line)
+		return (NULL);
+	copy_content_to_nl(list, line);
+	return (line);
+}
+
+static void	clear_list_till_nl(t_list **list)
+{
+	t_list	*last_node;
+	t_list	*clean_node;
+	int		i_con;
+	int		i_buf;
+	char	*buf;
+
+	buf = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (NULL == buf)
+		return ;
+	clean_node = (t_list *)malloc(sizeof(t_list));
+	if (NULL == clean_node)
+	{
+		free(buf);
+		return ;
+	}
+	last_node = ft_lstlast(*list);
+	i_con = 0;
+	i_buf = 0;
+	while (last_node->content[i_con] && last_node->content[i_con] != '\n')
+		i_con++;
+	while (last_node->content[i_con] && last_node->content[++i_con])
+		buf[i_buf++] = last_node->content[i_con];
+	buf[i_buf] = 0;
+	clean_node->content = buf;
+	dealloc_list(list, clean_node, buf);
+}
+
+char	*get_next_line(int fd)
+{
+	static t_list	*list[4096];
+	char			*next_line;
+
+	if (BUFFER_SIZE <= 0 || fd > 4095 || fd < 0 || read(fd, &next_line, 0) < 0)
+		return (NULL);
+	alloc_list(list, fd);
+	if (NULL == list[fd])
+		return (NULL);
+	next_line = get_line(list[fd]);
+	clear_list_till_nl(&list[fd]);
+	return (next_line);
+}
---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,121 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mamichal <mamichal@student.42warsaw.pl>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/24 12:01:01 by mamichal          #+#    #+#             */
+/*   Updated: 2024/04/03 16:19:29 by mamichal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "./get_next_line_bonus.h"
+
+int	found_nl(t_list *list)
+{
+	int	i;
+
+	if (NULL == list)
+		return (0);
+	while (list)
+	{
+		i = 0;
+		while (list->content[i] && i < BUFFER_SIZE)
+		{
+			if (list->content[i] == '\n')
+				return (1);
+			i++;
+		}
+		list = list->next;
+	}
+	return (0);
+}
+
+t_list	*ft_lstlast(t_list *lst)
+{
+	if (NULL == lst)
+		return (NULL);
+	while (NULL != lst->next)
+	{
+		lst = lst->next;
+	}
+	return (lst);
+}
+
+int	len_to_nl(t_list *list)
+{
+	int	i;
+	int	len;
+
+	if (NULL == list)
+		return (0);
+	len = 0;
+	while (list)
+	{
+		i = 0;
+		while (list->content[i])
+		{
+			if (list->content[i] == '\n')
+			{
+				len++;
+				return (len);
+			}
+			i++;
+			len++;
+		}
+		list = list->next;
+	}
+	return (len);
+}
+
+void	copy_content_to_nl(t_list *list, char *line)
+{
+	int	i_line;
+	int	i_content;
+
+	if (NULL == list || NULL == line)
+		return ;
+	i_line = 0;
+	while (list)
+	{
+		i_content = 0;
+		while (list->content[i_content])
+		{
+			if (list->content[i_content] == '\n')
+			{
+				line[i_line++] = '\n';
+				line[i_line] = 0;
+				return ;
+			}
+			line[i_line++] = list->content[i_content++];
+		}
+		list = list->next;
+	}
+	line[i_line] = 0;
+}
+
+void	dealloc_list(t_list **list, t_list *clean_node, char *buf)
+{
+	t_list	*tmp;
+
+	if (NULL != clean_node)
+		clean_node->next = NULL;
+	if (NULL == list)
+		return ;
+	while (*list)
+	{
+		tmp = (*list)->next;
+		free((*list)->content);
+		free(*list);
+		*list = tmp;
+	}
+	*list = NULL;
+	if (clean_node->content[0])
+		*list = clean_node;
+	else
+	{
+		free(buf);
+		free(clean_node);
+	}
+}
